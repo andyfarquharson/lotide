701123aaef4a3b40c0b8f4a04c91fc99e5aa56d1
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-17 23:21:02 +0000

    Added some cool emojis!

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🐸🐸🐸 Assertation Passed: " + actual + " === " + expected);
+  } else
+    console.log("🍄🍄🍄 Assertation Failed: " + actual + " !== " + expected);
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Farquharson", "Farquharson");
+assertEqual(45, 4.5);

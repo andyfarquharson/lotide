1ef60043abf6ff54289824b775dea5bd0bc673dc
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-18 01:34:36 +0000

    Takes the head of an array

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🐸🐸🐸 Assertation Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`🍄🍄🍄 Assertation Failed: ${actual} !== ${expected}`);
+};
+
+const head = function(input) {
+ 
+    return input[0];
+  };
+assertEqual(head([[5, 6, 7], 5]));
+assertEqual(head([["Hello" , "Lighthouse", "Labs"], "Hello"]));

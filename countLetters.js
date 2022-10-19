f7e40b981145d9b104eadeb0c5e3727ff4f9dc7f
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-19 20:51:08 +0000

    Counts letters in a string.  Doesn't count the spaces.

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🐸🐸🐸 Assertation Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`🍄🍄🍄 Assertation Failed: ${actual} !== ${expected}`);
+};
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("Farquharson", "Farquharson");
+// assertEqual(45, 4.5);
+
+const countLetters = function(sentence) {
+  let results = {};
+  sentence = sentence.replace(" ", "");
+  for (let char of sentence) {
+    // console.log(char);
+    if (!results[char]) {
+      results[char] = 1;
+    } else {
+      results[char]++;
+    }
+  }
+
+  return results;
+};
+
+const result1 = countLetters("LHL LHL");
+
+assertEqual(result1["L"], 4);
+assertEqual(result1["H"], 2);
+assertEqual(result1["a"], 1);

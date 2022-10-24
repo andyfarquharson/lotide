c84021f338c066340b8417c29cffa0ed259c85ef
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-24 22:43:29 +0000

    Forgot to delete obsolete comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,6 +12,3 @@ describe('#middle', () => {
     assert.deepStrictEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);

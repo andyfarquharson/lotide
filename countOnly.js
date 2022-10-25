0e943e1bfc7c8db2be0814b4bf18bcc7fac151fb
Andy Farquharson <andyfarquharson@gmail.com>
2022-10-25 00:51:51 +0000

    Seperated out testing code into a seperate forlder

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🐸🐸🐸 Assertation Passed: ${actual} === ${expected}`);
-  } else
-    console.log(`🍄🍄🍄 Assertation Failed: ${actual} !== ${expected}`);
-};
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Farquharson", "Farquharson");
-// assertEqual(45, 4.5);
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
     // console.log(item);
     if (itemsToCount[item]) {
@@ -23,24 +11,6 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-
-
 }
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly.js
